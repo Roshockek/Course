cd59bf32e07399e487b6d89097563698cbfb8166
Roshockek <95378461+Roshockek@users.noreply.github.com>
2024-04-29 10:46:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Привет мир!');
+// Указываем путь к файлу cicd.html
+const cicdPagePath = path.join(__dirname, 'cicd.html');
+
+// Обработка GET запроса на страницу с описанием CI/CD
+app.get('/cicd', (req, res) => {
+  res.sendFile(cicdPagePath);
 });
 
 app.listen(port, '0.0.0.0', () => {
